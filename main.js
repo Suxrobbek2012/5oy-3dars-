d5a12cffaf5b6627044671903673dbbbeb8605d8
Suxrobbek2012 <suxrobbek714@gmail.com>
2025-02-06 11:39:00 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,115 @@
+// var string = prompt("string kriting");
+// console.log(string);
+
+
+// var number = +prompt("number kriting");
+// console.log(number);
+
+// var bolian = prompt("bolian kriting");
+// console.log(bolian);
+
+// var togrimi = prompt("hama malumot togrimi");
+// console.log(togrimi);
+
+
+
+
+
+
+
+
+
+// alert(`stringi tegishli:${string}
+//     numberi tegishli:${number}
+//     boliani tegishli:${bolian}`);
+
+// var bot = prompt("botmisan");
+// console.log(bot);
+// const num1 = +prompt("1sonni kriting");
+// const num2 = +prompt("2sonni kriting");
+// console.log(`${num1} + ${num} = ${num1} + ${num}`)
+
+
+// const (imitihonball = 75);
+// if(imitihonball == 75){
+//   alert( " tabriklaymiz siz o'ttingiz")
+// }else{
+//     alert("botsan")
+// }
+// const imitihonball == 25;
+
+// if(imitihonball == 25){
+//   alert( " tugadi")
+// }else{
+//     alert("oldin")
+// }
+// const botsan = +prompt("90")
+// const oldin = +prompt("20")
+
+
+
+// let num = 17.4;
+
+//const result = Math.floor(num);
+
+//console.log(result);
+
+//const result = Math.ceil(num);
+
+//console.log(result);
+
+//const result = Math.round(num);
+
+//console.log(result);
+
+// const randomRaqam = Math.round(Math.random() * 50 );
+// console.log(randomRaqam);
+
+// const num = +prompt("raqam kiriting");
+
+// if(num % 2 == 0){
+//     alert(`%{num} soni juft`)
+// }else{
+//     alert(`%{num} soni toq`)
+// }
+
+
+
+
+
+alert("Sizda hozir random sonlar chiqishni boshlaydi 😎😎");
+let num = Math.round(Math.random() *50);
+alert(`Random orqali chiqqan son : ${num}`);
+confirm("Biz random sonni 3 ga kopaytirmoqchi va bo'lmoqchimiz !");
+alert(`Random orqali chiqqan son: ${num} edi,
+    Biz uni 2 ga ko'paytirib: ${num * 3} sonini chiqardik,
+    va shu sonni 2 ga bo'lib: ${num / 2} sonini chiqardik,
+    va shu sonni 3 ga bolgandagi qoldiq: ${num % 3} sonini chiqardik`
+)
+
+
+
+
+
+
+
+
+
+// const num = +prompt("1-raqamni kriting");
+
+// const num2 = +prompt("2-raqamni kriting");
+
+// const operator = +prompt("1) qoshish \n 2)ayirish  \n 3)kopaytirish  \n 4)bolish");
+
+// if(operator == 1){
+//     alert(`javob:${num + num2}`)
+// }else
+// if(operator == 2){
+//     alert(`javob:${num + num2}`)
+// }else
+// if(operator == 3){
+//     alert(`javob:${num + num2}`)
+// }else
+// if(operator == 4){
+//     alert(`javob:${num + num2}`)
+// }
